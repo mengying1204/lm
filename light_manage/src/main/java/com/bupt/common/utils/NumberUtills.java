cf2a66f52efb22e09d99935565143b1e137fcc8c
mengying1204 <985216509@qq.com>
2017-09-27 10:36:18 +0800

    Merge branches 'gmy' and 'master' of https://github.com/xingchen0305/lm into gmy
    # Conflicts:
    #	.idea/libraries/Maven__org_assertj_assertj_core_2_5_0.xml
    #	.idea/libraries/Maven__org_json_json_20140107.xml
    #	.idea/libraries/Maven__org_skyscreamer_jsonassert_1_3_0.xml
    #	light_manage/pom.xml

--- light_manage/src/main/java/com/bupt/common/utils/NumberUtills.java
+++ light_manage/src/main/java/com/bupt/common/utils/NumberUtills.java
@@ -1,7 +1,34 @@
 package com.bupt.common.utils;
 
+import java.util.Date;
+
 /**
  * Created by mengying on 2017/9/26.
  */
-public class demo {
+public class NumberUtills {
+
+    public static String getNumber() {
+        String seqID = "";
+        seqID+= getNO();
+        seqID += String.valueOf(RandomI(0, 9));
+        seqID += String.valueOf(RandomI(0, 9));
+        return seqID;
+    }
+
+    public static int RandomI(int m, int n) {
+        double j = Math.round(Math.random() * (n - m));
+        return (int) (j + m);
+    }
+
+    public static String getNO() {
+        Long date = new Date().getTime();
+        String time = date.toString();
+        return time;
+    }
+
+
+
+
+
+
 }
